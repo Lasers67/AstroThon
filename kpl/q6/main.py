40ae8d5f54ebfe26f273d5a00f0dbd30edfef545
Pratyush Gaurav <b16026@students.iitmandi.ac.in>
2020-06-08 22:56:46 +0530

    q6 added

--- kpl/q6/main.py
+++ kpl/q6/main.py
@@ -10,7 +10,7 @@ def findstrike(v, alt, az):
 	alt=math.radians(alt)
 	G=6.673*(10**-11)
 	M=5.97219*(10**24)
-    Re=6371000
+        Re=6371000.0
 	Ri=2*Re*G*M/(2*G*M-((v*math.sin(alt))**2)*Re)
 	t=((2*(Re**3)/(G*M))**0.5)*(math.atan((Re/(Ri-Re))**0.5)-((Re*(Ri-Re))/Ri)**0.5+math.pi/2)
 	d= v*math.cos(alt)*t
