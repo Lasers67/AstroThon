44e0dfb0af42ffff601dfa965bf62a5e1d5d245d
khyati18 <agarwal.khyati5@gmail.com>
2020-06-08 23:29:33 +0530

    q4

--- kpl/q4/main.py
+++ kpl/q4/main.py
@@ -14,8 +14,8 @@ Any extra lines of code (if required)
 as helper for this function.
 """
 
-startobs = datetime(2000, 1, 1, 0, 0, 0) #replace it by the time when Saturn will be just visible
-endobs = datetime(2020, 1, 1) #replace it by the time when Saturn is no longer visible from SAC terrace
+startobs = datetime(2020, 6, 9, 0, 10, 0) #replace it by the time when Saturn will be just visible
+endobs = datetime(2020, 6, 9, 6 ,36 , 0) #replace it by the time when Saturn is no longer visible from SAC terrace
 
 def findSaturn(obstime):
 	'''
@@ -28,54 +28,21 @@ def findSaturn(obstime):
 	A `tuple` of two floats.
 	'''
 
+	saturn = ephem.Saturn()
+	saturn.compute(obstime)
+	body = ephem.FixedBody()
+	body._ra = saturn.ra
+	body._dec = saturn.dec
+	Gravity = ephem.Observer()
+	Gravity.lat = '31.7754'
+	Gravity.lon = '76.9861'
+	Gravity.elevation = 1000
+	Gravity.date = obstime
+	body.compute(Gravity)
 
-	m33 = SkyCoord.from_name('M33')
-	bear_mountain = EarthLocation(lat=31.7754*u.deg, lon=-76.9861*u.deg, height=1000*u.m)
-	utcoffset = -4*u.hour  # Eastern Daylight Time
-	time = Time('2012-7-12 23:00:00') - utcoffset
-
-	m33altaz = m33.transform_to(AltAz(obstime=time,location=bear_mountain))
-	# print("M33's Altitude = {0.alt:.2}".format(m33altaz))
-
-	print(m33altaz.alt, m33altaz.az)
-
-	# saturn = ephem.Saturn()
-	# saturn.compute(obstime)
-	# body = ephem.FixedBody()
-	# body._ra = saturn.ra
-	# body._dec = saturn.dec
-	# Gravity = ephem.Observer()
-	# Gravity.lat = '31.7754'
-	# Gravity.lon = '76.9861'
-	# Gravity.date = obstime
-	# body.compute(Gravity)
-
-	# return((body.alt, body.az))
-
-# print(findSaturn(startobs))
-# current = startobs
-# i=0
-# while(current<endobs):
-# 	altitude = math.degrees(findSaturn(current)[0])
-# 	if(altitude>=-20):
-# 		startobs = current
-# 	current = current + timedelta(hours=1)
-# 	i+=1
-
-# current = startobs
-# i=0
-# while(current<endobs):
-# 	altitude = math.degrees(findSaturn(current)[0])
-# 	if(altitude>=-160):
-# 		endobs = current
-# 	current = current + timedelta(hours=1)
-# 	i+=1
+	return((body.alt, body.az))
 
 
-print(startobs)
+
 print(findSaturn(startobs))
-# print(math.degrees(findSaturn(startobs)[0]))
-
-# print(endobs)
-# print(findSaturn(endobs))
-# print(math.degrees(findSaturn(endobs)[0]))
+print(findSaturn(endobs))
