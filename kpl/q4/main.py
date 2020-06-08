34d3c54822dc354c6af746bfa5915e38915afdbb
khyati18 <agarwal.khyati5@gmail.com>
2020-06-08 22:31:25 +0530

    q4

--- kpl/q4/main.py
+++ kpl/q4/main.py
@@ -1,6 +1,8 @@
 import ephem
 from datetime import datetime
 from datetime import time
+from datetime import timedelta
+import math 
 
 """
 Any extra lines of code (if required)
@@ -30,6 +32,33 @@ def findSaturn(obstime):
 	Gravity.lon = '76.9861'
 	Gravity.date = obstime
 	body.compute(Gravity)
-	return((body.az, body.alt))
 
+	return((body.alt, body.az))
+
+# print(findSaturn(startobs))
+current = startobs
+i=0
+while(current<endobs):
+	altitude = math.degrees(findSaturn(current)[0])
+	if(altitude>=-20):
+		startobs = current
+	current = current + timedelta(hours=1)
+	i+=1
+
+current = startobs
+i=0
+while(current<endobs):
+	altitude = math.degrees(findSaturn(current)[0])
+	if(altitude>=-160):
+		endobs = current
+	current = current + timedelta(hours=1)
+	i+=1
+
+
+print(startobs)
 print(findSaturn(startobs))
+print(math.degrees(findSaturn(startobs)[0]))
+
+print(endobs)
+print(findSaturn(endobs))
+print(math.degrees(findSaturn(endobs)[0]))
